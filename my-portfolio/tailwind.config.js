6dcbfbf9f4e96cba1d1151911bf489e68d91ccf1
EF-Pami <pamijoelpami@gmail.com>
2024-10-01 22:22:13 +0200

    added tailwind.config.js

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default  {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    colors: {
+      darkblue: "#152737",
+      belge: "#FCEBEB",
+      white: "#fff",
+      black: "#000",
+      arch: "#D9D9D9",
+      
+    },
     extend: {},
   },
   plugins: [],
-}
+};
 
