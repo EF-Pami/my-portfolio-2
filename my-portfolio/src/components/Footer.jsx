b7f4f1877439232f41f084c782d9dc35879c7459
EF-Pami <pamijoelpami@gmail.com>
2024-10-01 22:18:40 +0200

    added footer section

---
+++ my-portfolio/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-darkblue text-white text-center py-4">
+      <p>&copy; Copyright 2024 EF II PAMI-PAMI. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
