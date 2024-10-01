72e1c97dfe6e108046ee8508cbfb29bddd307e9e
EF-Pami <pamijoelpami@gmail.com>
2024-10-01 22:15:29 +0200

    added about section

---
+++ my-portfolio/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about" className="bg-darkblue text-white py-20">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-bold mb-5">About Me</h2>
+        <p className="max-w-xl mx-auto">
+        Passionate front-end developer with a strong foundation in creating responsive, user-friendly web applications. Proficient in HTML, CSS, JavaScript, and React, with experience in modern design principles and an eye for detail. Excited to bring creative solutions to real-world challenges.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
